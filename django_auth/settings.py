09d667e29cace8a1a751c72d2919f889c6d1e088
cachimiro <johannaguirre55@gmail.com>
2020-01-29 13:30:18 +0000

    adding products

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
+
+if os.path.exists('env.py'):
+    import env
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -66,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -82,6 +88,21 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    print("Postgres URL not found, using sqlite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
@@ -125,6 +146,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
--- information/urls.py
+++ information/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
-from.views import info
+from .views import info, info_detail, create_or_edit_info
+
+
 
 urlpatterns = [
     url(r'^info/', info, name="info"),
+    url(r'^(?P<pk>\d+)/$', info_detail, name='info_detail'),
+    url(r'^new/$', create_or_edit_info, name='new_info'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_info, name='edit_info')
     
     ]
