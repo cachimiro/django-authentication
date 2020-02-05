4a75fd6dbdb8efae185ccd24da16f1adee5c26f4
cachimiro <johannaguirre55@gmail.com>
2020-02-05 13:55:24 +0000

    fixing bugs and developing the index.html

--- django_auth/urls.py
+++ django_auth/urls.py
@@ -17,17 +17,9 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
-from information import urls as information_urls
-from django.views.generic import RedirectView
-from django.views.static import serve
-from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
-    url(r'^accounts/', include(accounts_urls)),
-    url(r'^information/', include(information_urls)),
-     url(r'^$', RedirectView.as_view(url='posts/')),
-    url(r'^posts/', include('post.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
+    url(r'^accounts/', include(accounts_urls))
 ]
