d47c521bcaeadd9c9addae317f7eaa2adfef1053
cachimiro <johannaguirre55@gmail.com>
2020-05-27 16:18:10 +0000

    adding some style to the product page

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-import uuid
+
+
 
 # Create your models here.
 class Product(models.Model):
@@ -13,6 +14,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-def randon_id_generator():
-    uuid.uuid4()
-    return render(request,'products.html')
+
+
+
